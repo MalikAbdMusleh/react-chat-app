62a6ccf06ae79911c4912d9863ed185fa9b30bde
MalikAbdMusleh <malikabdmusleh@gmail.com>
2021-11-02 20:04:28 +0200

    the pages saperated

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import ChatBox from './chat-box'
 import './App.css';
 
 class Home extends React.Component {
@@ -6,214 +9,43 @@ class Home extends React.Component {
     super();
     this.state = {
       name: "unknown",
-      massage: "",
-      allMassages: [{ massage: "aaaaaaaa" }, { massage: "xxxxaxaxa", }]
     };
   }
-  addMassage(mm) {
-    let old =this.state.allMassages
-    console.log(old,"old");
-    old.push(mm)
-    this.setState({ allMassages: old })
+
+  getName(val) {
+
   }
+
   render() {
 
     return (
+      <Router>
+        <Route path="/chat-box"  >
+          <ChatBox name={this.state.name} />
+        </Route>
+        <div className="maincontainer">
+          <div class="container py-5 px-4" style={{ display: 'flex', flexWrap: 'wrap', flexFlow: "column", alignItems: 'center', borderTop: "5px black solid", marginTop: "100px", boxShadow: "15px 115px 115px 15px " }}>
 
-      <div className="maincontainer">
-        <div class="container py-5 px-4">
+            <header class="text-center">
+              <h1 class="display-4 text-white"><strong>Malik and Super Chat</strong></h1>
+              <p class="text-white lead mb-4">Snippet by
+                <a href="#" class="text-white">
+                  <u> Eng Malik </u></a>
+              </p>
+            </header>
 
-          <header class="text-center">
-            <h1 class="display-4 text-white"><strong>Malik and lili Chat</strong></h1>
-            {/* <p class="text-white lead mb-0">An elegant chat widget compatible with <strong>Reactjs Bootstrap 4</strong></p> */}
-            <p class="text-white lead mb-4">Snippet by
-              <a href="#" class="text-white">
-                <u> Eng Malik </u></a>
-            </p>
-          </header>
-          <div class="row rounded-lg overflow-hidden shadow">
-
-            <div class="col-5 px-0">
-              <div class="bg-white">
-                <div class="bg-gray px-4 py-2 bg-light">
-                  <p class="h5 mb-0 py-1">Recent</p>
-                </div>
-                <div class="messages-box">
-                  <div class="list-group rounded-0">
-                    <a class="list-group-item list-group-item-action active text-white rounded-0">
-                      <div class="media"><img src="https://therichpost.com/wp-content/uploads/2020/06/avatar2.png" alt="user" width="50" class="rounded-circle" />
-                        <div class="media-body ml-4">
-                          <div class="d-flex align-items-center justify-content-between mb-1">
-                            <h6 class="mb-0">Jassa</h6><small class="small font-weight-bold">25 Dec</small>
-                          </div>
-                          <p class="font-italic mb-0 text-small">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                        </div>
-                      </div>
-                    </a>
-                    <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-                      <div class="media"><img src="https://therichpost.com/wp-content/uploads/2020/06/avatar2.png" alt="user" width="50" class="rounded-circle" />
-                        <div class="media-body ml-4">
-                          <div class="d-flex align-items-center justify-content-between mb-1">
-                            <h6 class="mb-0">Jassa</h6><small class="small font-weight-bold">14 Dec</small>
-                          </div>
-                          <p class="font-italic text-muted mb-0 text-small">Lorem ipsum dolor sit amet, consectetur. incididunt ut labore.</p>
-                        </div>
-                      </div>
-                    </a>
-                    <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-                      <div class="media"><img src="https://therichpost.com/wp-content/uploads/2020/06/avatar2.png" alt="user" width="50" class="rounded-circle" />
-                        <div class="media-body ml-4">
-                          <div class="d-flex align-items-center justify-content-between mb-1">
-                            <h6 class="mb-0">Jassa</h6><small class="small font-weight-bold">9 Nov</small>
-                          </div>
-                          <p class="font-italic text-muted mb-0 text-small">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                        </div>
-                      </div>
-                    </a>
-                    <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-                      <div class="media"><img src="https://therichpost.com/wp-content/uploads/2020/06/avatar2.png" alt="user" width="50" class="rounded-circle" />
-                        <div class="media-body ml-4">
-                          <div class="d-flex align-items-center justify-content-between mb-1">
-                            <h6 class="mb-0">Jassa</h6><small class="small font-weight-bold">18 Oct</small>
-                          </div>
-                          <p class="font-italic text-muted mb-0 text-small">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                        </div>
-                      </div>
-                    </a>
-                    <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-                      <div class="media"><img src="https://therichpost.com/wp-content/uploads/2020/06/avatar2.png" alt="user" width="50" class="rounded-circle" />
-                        <div class="media-body ml-4">
-                          <div class="d-flex align-items-center justify-content-between mb-1">
-                            <h6 class="mb-0">Jassa</h6><small class="small font-weight-bold">17 Oct</small>
-                          </div>
-                          <p class="font-italic text-muted mb-0 text-small">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                        </div>
-                      </div>
-                    </a>
-                    <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-                      <div class="media"><img src="https://therichpost.com/wp-content/uploads/2020/06/avatar2.png" alt="user" width="50" class="rounded-circle" />
-                        <div class="media-body ml-4">
-                          <div class="d-flex align-items-center justify-content-between mb-1">
-                            <h6 class="mb-0">Jassa</h6><small class="small font-weight-bold">2 Sep</small>
-                          </div>
-                          <p class="font-italic text-muted mb-0 text-small">Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        </div>
-                      </div>
-                    </a>
-                    <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-                      <div class="media"><img src="https://therichpost.com/wp-content/uploads/2020/06/avatar2.png" alt="user" width="50" class="rounded-circle" />
-                        <div class="media-body ml-4">
-                          <div class="d-flex align-items-center justify-content-between mb-1">
-                            <h6 class="mb-0">Jassa</h6><small class="small font-weight-bold">30 Aug</small>
-                          </div>
-                          <p class="font-italic text-muted mb-0 text-small">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                        </div>
-                      </div>
-                    </a>
-                    <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-                      <div class="media"><img src="https://therichpost.com/wp-content/uploads/2020/06/avatar2.png" alt="user" width="50" class="rounded-circle" />
-                        <div class="media-body ml-4">
-                          <div class="d-flex align-items-center justify-content-between mb-3">
-                            <h6 class="mb-0">Jassa</h6><small class="small font-weight-bold">21 Aug</small>
-                          </div>
-                          <p class="font-italic text-muted mb-0 text-small">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                </div>
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="">First and last name</span>
               </div>
-            </div>
-
-            <div class="col-7 px-0">
-              <div class="px-4 py-5 chat-box bg-white">
-
-                {/* <div class="media w-50 mb-3"><img src="https://therichpost.com/wp-content/uploads/2020/06/avatar2.png" alt="user" width="50" class="rounded-circle" />
-                  <div class="media-body ml-3">
-                    <div class="bg-light rounded py-2 px-3 mb-2">
-                      <p class="text-small mb-0 text-muted">Test which is a new approach all solutions</p>
-                    </div>
-                    <p class="small text-muted">12:00 PM | Aug 13</p>
-                  </div>
-                </div> */}
-                {this.state.allMassages.map((e) => <div>
-                  <div class="media w-50 mb-3"><img src="https://therichpost.com/wp-content/uploads/2020/06/avatar2.png" alt="user" width="50" class="rounded-circle" />
-                    <div class="media-body ml-3">
-                      <div class="bg-light rounded py-2 px-3 mb-2">
-                        <p class="text-small mb-0 text-muted">{this.state.name}</p>
-                      </div>
-                      <p class="small text-muted">12:00 PM | Aug 13</p>
-                    </div>
-                  </div>
-                  <div class="media w-50 ml-auto mb-3">
-                    <div class="media-body">
-                      <div class="bg-primary rounded py-2 px-3 mb-2">
-                        <p class="text-small mb-0 text-white">{e.massage}</p>
-                      </div>
-                      <p class="small text-muted">12:00 PM | Aug 13</p>
-                    </div>
-                  </div>
-                </div>)}
-                <div class="media w-50 ml-auto mb-3">
-                  <div class="media-body">
-                    <div class="bg-primary rounded py-2 px-3 mb-2">
-                      <p class="text-small mb-0 text-white">Test which is a new approach to have all solutions</p>
-                    </div>
-                    <p class="small text-muted">12:00 PM | Aug 13</p>
-                  </div>
-                </div>
-
-                <div class="media w-50 mb-3"><img src="https://therichpost.com/wp-content/uploads/2020/06/avatar2.png" alt="user" width="50" class="rounded-circle" />
-                  <div class="media-body ml-3">
-                    <div class="bg-light rounded py-2 px-3 mb-2">
-                      <p class="text-small mb-0 text-muted">Test, which is a new approach to have</p>
-                    </div>
-                    <p class="small text-muted">12:00 PM | Aug 13</p>
-                  </div>
-                </div>
-                {/* this is how the massage look like  */}
-                {/* <div class="media w-50 ml-auto mb-3">
-                  <div class="media-body">
-                    <div class="bg-primary rounded py-2 px-3 mb-2">
-                      <p class="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
-                    </div>
-                    <p class="small text-muted">12:00 PM | Aug 13</p>
-                  </div>
-                </div> */}
-                {/* to here */}
-                {/* <div class="media w-50 mb-3"><img src="https://therichpost.com/wp-content/uploads/2020/06/avatar2.png" alt="user" width="50" class="rounded-circle" />
-                  <div class="media-body ml-3">
-                    <div class="bg-light rounded py-2 px-3 mb-2">
-                      <p class="text-small mb-0 text-muted">Test, which is a new approach</p>
-                    </div>
-                    <p class="small text-muted">12:00 PM | Aug 13</p>
-                  </div>
-                </div>
-
-                <div class="media w-50 ml-auto mb-3">
-                  <div class="media-body">
-                    <div class="bg-primary rounded py-2 px-3 mb-2">
-                      <p class="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
-                    </div>
-                    <p class="small text-muted">12:00 PM | Aug 13</p>
-                  </div>
-                </div>*/}
-              </div>
-
-              <form action="#" class="bg-light">
-                <div class="input-group">
-                  <input type="text" onChange={(e) => this.setState({ massage: { massage: e.target.value } })} placeholder="Type a message" aria-describedby="button-addon2" class="form-control rounded-0 border-0 py-4 bg-light" />
-                  <div class="input-group-append">
-                    <button id="button-addon2" onClick={() => this.addMassage(this.state.massage)} type="button" class="btn btn-link"> <i class="fa fa-paper-plane"></i></button>
-                  </div>
-                </div>
-              </form>
+              <input type="text" class="form-control" onChange={(e) => this.setState({ name: e.target.value })} />
+              <input type="text" class="form-control" />
+              <a href="http://localhost:3000/chat-box" ><button  id="button-addon2" className="btn btn-primary"  >Okey !!</button>
+              </a>
             </div>
           </div>
         </div>
-      </div>
-
-
+      </Router>
     )
   };
 }
